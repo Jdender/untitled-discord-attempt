4155273e336d8df1eaad4c3218a26564a075f796
Jdender~ <jdenderplays@gmail.com>
2020-05-13 21:35:29 -0400

    Add next and nexus

---
+++ graphql/schema.ts
@@ -0,0 +1,12 @@
+import { schema } from 'nexus'
+
+schema.queryType({
+  definition(t) {
+    t.field("ping", {
+      type: 'String',
+      async resolve(_root, args, ctx) {
+        return 'Pong!';
+      }
+    })
+  }
+})
