4760ef000e75d27a8159faf68dafa4de49c7ae31
Jdender~ <jdenderplays@gmail.com>
2020-05-18 15:23:43 -0400

    Add graphql code genrator for apollo hooks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
 import React, { FC } from 'react';
+import { useTodosQuery } from '../client-util/generated';
 
-const Index: FC = () => <h1>Hello world!</h1>;
+const Index: FC = () => {
+    const { data, loading, error } = useTodosQuery();
+
+    if (!data || loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {JSON.stringify(error)}</p>;
+
+    return (
+        <ul>
+            {data.todos.map((todo, id) => (
+                <li key={id}>{todo.description}</li>
+            ))}
+        </ul>
+    );
+};
 
 export default Index;
