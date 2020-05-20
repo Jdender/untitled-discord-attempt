6a68984e49042b19068c00ef7236451e7be6c89a
Jdender~ <jdenderplays@gmail.com>
2020-05-20 11:52:23 -0400

    Update login to use util file

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,24 @@
 import { NowRequest, NowResponse } from '@now/node';
-import fetch from 'isomorphic-unfetch';
+import { getToken, getUser, getGuilds, OAUTH_URL } from '../../util/discord';
 
 export default async (req: NowRequest, res: NowResponse) => {
-    const tokenRes = fetch('https://discordapp.com/api/oauth2/token', {
-        method: 'POST',
-        body: JSON.stringify({
-            code: req.query.code,
-            scope: 'identity email guilds',
-            /* eslint-disable @typescript-eslint/camelcase */
-            client_id: process.env.DISCORD_OAUTH_ID,
-            client_secret: process.env.DISCORD_OAUTH_SECRET,
-            grant_type: 'authorization_code',
-            redirect_url: 'https://untitled-discord-attempt.now.sh/api/login',
-            /* eslint-enable @typescript-eslint/camelcase */
-        }),
-    }).then((res) => res.json());
-    res.json(tokenRes);
+    const code = req.query.code;
+
+    if (typeof code !== 'string') {
+        res.writeHead(302, { Location: OAUTH_URL }).end();
+        return;
+    }
+
+    const accessToken = (await getToken(code, 'grant'))?.access_token;
+
+    if (typeof accessToken !== 'string') {
+        res.status(400).send('OAuth grant failed.');
+        res.end();
+        return;
+    }
+
+    const userRes = await getUser(accessToken);
+    const guildsRes = await getGuilds(accessToken);
+
+    res.json({ userRes, guildsRes });
 };
