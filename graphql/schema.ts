b69acc4a8bfeda9c6f9d97c1012a1cbef70d5826
Jdender~ <jdenderplays@gmail.com>
2020-05-21 19:06:14 -0400

    Add new graphql types and add an double-init guard to the Nexus app

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,7 +1,14 @@
 import { settings, schema, use } from 'nexus';
 import { prisma } from 'nexus-plugin-prisma';
 
-use(prisma());
+const HAS_NEXUS_INIT_KEY = Symbol('HAS_NEXUS_INIT');
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+if (!(globalThis as any)[HAS_NEXUS_INIT_KEY]) {
+    use(prisma());
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (globalThis as any)[HAS_NEXUS_INIT_KEY] = true;
+}
 
 settings.change({
     schema: { generateGraphQLSDLFile: './.generated/schema.gql' },
@@ -10,28 +17,23 @@ settings.change({
 schema.interfaceType({
     name: 'Node',
     definition(t) {
-        t.id('id');
+        t.id('id', { nullable: false });
         t.resolveType(() => null);
     },
 });
 
 schema.objectType({
-    name: 'Todo',
+    name: 'ServerEntry',
     definition(t) {
         t.implements('Node');
-        t.model.description();
+        t.model.name();
+        t.model.icon();
+        t.model.features();
     },
 });
 
 schema.queryType({
     definition(t) {
-        t.crud.todos();
-    },
-});
-
-schema.mutationType({
-    definition(t) {
-        t.crud.createOneTodo();
-        t.crud.deleteOneTodo();
+        t.crud.serverEntries();
     },
 });
