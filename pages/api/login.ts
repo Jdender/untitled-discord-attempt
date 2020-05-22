30604452d8075eb589b32d3c6522fb80b3e5ce51
Jdender~ <jdenderplays@gmail.com>
2020-05-22 15:55:54 -0400

    Add test code to login

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,8 @@
-import { getToken, getUser, OAUTH_URL } from '../../util/discord';
+import { getToken, getUser, OAUTH_URL, getGuilds } from '../../util/discord';
 import { PrismaClient } from '@prisma/client';
 import { handleResult } from '../../util/requestHandlers';
 import { expect } from '../../util/generic/Result';
+import { I } from '../../util/generic/Identity';
 
 const db = new PrismaClient();
 
@@ -42,6 +43,53 @@ export default handleResult(400, async (req, res) => {
             id: userRes.id,
         },
     });
+
+    const guilds = (await getGuilds(tokenRes.access_token)).expect(
+        'Failed to get guilds.',
+    );
+
+    expect(
+        process.env.NODE_ENV !== 'production',
+        'You forgot to remove the test code idiot.',
+    );
+
+    // TODO: Remove the following
+    await I(guilds)
+        .map((guilds) =>
+            guilds
+                // .filter(({ owner }) => owner)
+                .map(({ id, name, icon, features }) =>
+                    db.serverEntry.upsert({
+                        create: {
+                            id,
+                            owner: {
+                                connect: {
+                                    id: userRes.id,
+                                },
+                            },
+                            name,
+                            description: 'TODO',
+                            icon,
+                            features: {
+                                set: features,
+                            },
+                        },
+                        update: {
+                            id,
+                            name,
+                            icon,
+                            features: {
+                                set: features,
+                            },
+                        },
+                        where: {
+                            id,
+                        },
+                    }),
+                ),
+        )
+        .map((guilds) => Promise.all(guilds))
+        .unwrap();
 
     res.json({ userRes });
 });
