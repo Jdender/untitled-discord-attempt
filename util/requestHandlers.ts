7b675e458aad92d70c402de09b11fd6acba8022b
Jdender~ <jdenderplays@gmail.com>
2020-05-21 20:23:48 -0400

    Add requestHandlers util module

---
+++ util/requestHandlers.ts
@@ -0,0 +1,17 @@
+import { NowRequest, NowResponse } from '@now/node';
+import { ResultExpectError } from './Result';
+
+export type RequestHandler = (req: NowRequest, res: NowResponse) => void;
+
+export const handleResult = (
+    code: number,
+    handle: RequestHandler,
+): RequestHandler => (req, res) => {
+    try {
+        return handle(req, res);
+    } catch (e) {
+        if (!(e instanceof ResultExpectError)) throw e;
+        res.status(code).send(e.message);
+        res.end();
+    }
+};
