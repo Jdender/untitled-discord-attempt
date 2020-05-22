502d7233a33ad43919237a92456c36f5505753ce
Jdender~ <jdenderplays@gmail.com>
2020-05-22 15:55:28 -0400

    Update index page to use updated schema and new Card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { useEntriesQuery } from '../.generated/hooks';
+import Card from '../components/Card';
 
 const Index: FC = () => {
     const { data, loading, error } = useEntriesQuery();
@@ -10,13 +11,28 @@ const Index: FC = () => {
     return (
         <div>
             <a href="/api/login">Login</a>
-            <ul>
+            <main>
                 {data.serverEntries.map((entry) => (
-                    <li key={entry.id}>
-                        {entry.name} {entry.features}
-                    </li>
+                    <Card
+                        key={entry.id}
+                        icon={
+                            entry.icon == null
+                                ? 'https://discord.com/assets/f8389ca1a741a115313bede9ac02e2c0.svg'
+                                : `https://cdn.discordapp.com/icons/${entry.id}/${entry.icon}.png`
+                        }
+                        header={entry.name}
+                        content={entry.description}
+                    />
                 ))}
-            </ul>
+            </main>
+            <style jsx>{`
+                main {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: space-evenly;
+                    align-items: center;
+                }
+            `}</style>
         </div>
     );
 };
